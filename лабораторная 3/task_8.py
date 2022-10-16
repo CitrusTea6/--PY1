9b651d51228d569438e823db771b6075952e5f5b
CitrusTea6 <115555799+CitrusTea6@users.noreply.github.com>
2022-10-16 19:03:36 +0300

    Лабораторная работа 3

---
+++ лабораторная 3/task_8.py
@@ -0,0 +1,13 @@
+money_capital = 10000
+salary = 5000
+spend = 6000
+increase = 0.05
+
+month = 0
+grow = increase + 1
+delta = salary - spend
+while money_capital + delta > spend:
+    money_capital += delta
+    spend = spend * grow
+    month += 1
+print(month)
